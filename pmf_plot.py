5fe7d73be6f0b893064e07a683a24ebd1b6eb3c4
cn29 <cnzhang@ucdavis.edu>
2018-11-02 21:06:49 -0700

    pmf plot v2

--- pmf_plot.py
+++ pmf_plot.py
@@ -3,11 +3,11 @@ import numpy as np
 import csv
 
 
-def readCSV(file_name, plot_num=6):
+def readCSV(file_name, plot_num):
     print(1)
     with open(file_name) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
-        row_count = 31*2
+        row_count = 29*2
         line_count = 0
         tickers = []
         data_matrix = np.zeros([plot_num, row_count], dtype=float)
@@ -47,11 +47,29 @@ def plotData(tickers, data_matrix, plot_name, figure_title, y_label):
     plt.show()
 
 
-if __name__ == '__main__':
-    file_name = 'sp.csv'
-    plot_name = ['Wood burning/\nmeat cooking', 'Traffic', 'Shipping', 'Sea salt/dust', 'Sn', 'Sb']
-    figure_title = 'Title!'
-    y_label = 'Label!'
+def spot_select(spot):
+    if spot == 'EO':
+        file_name = 'EO.csv'
+        plot_name = ['Wood burning', 'Traffic/meat cooking', 'EC3', 'Shipping', 'Sea salt/dust', 'Sn', 'Sb']
+        figure_title = 'East Oakland'
+        y_label = 'Concentration $(\mu g/m^3)$'
+        plot_num = 7
+    elif spot == 'SP':
+        file_name = 'SP.csv'
+        plot_name = ['Wood burning/meat cooking', 'Traffic', 'Shipping', 'Sea salt/dust', 'Sn', 'Sb']
+        figure_title = 'San Pablo'
+        y_label = 'Concentration $(\mu g/m^3)$'
+        plot_num = 6
+    else:
+        file_name = 'LA.csv'
+        plot_name = ['Wood burning/meat cooking', 'Traffic', 'Shipping', 'Sea salt/dust', 'Sn', 'Sb']
+        figure_title = 'Los Angeles'
+        y_label = 'Concentration $(\mu g/m^3)$'
+        plot_num = 6
+    return file_name, plot_name, figure_title, y_label, plot_num
 
-    tickers, data_matrix, line_count = readCSV(file_name)
+if __name__ == '__main__':
+    file_name, plot_name, figure_title, y_label, plot_num = spot_select('LA')
+
+    tickers, data_matrix, line_count = readCSV(file_name, plot_num)
     plotData(tickers, data_matrix[:,:line_count], plot_name[::-1], figure_title, y_label)
